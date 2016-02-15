408f1d9a4e772bba5638e541424542f9aead0e59
Jan Kristinus <jan.kristinus@yakamara.de>
2016-02-15 17:39:43 +0100

    Manager: Sortierung der Tabellen in der Navi nach Prio fixed #7

--- plugins/manager/boot.php
+++ plugins/manager/boot.php
@@ -17,14 +17,17 @@ if (rex::isBackend() && rex::getUser()) {
     $tables = rex_yform_manager_table::getAll();
     $pages = [];
 
+    $prio = 1;
     foreach ($tables as $table) {
         $table_perm = 'yform[table:' . $table['table_name'] . ']';
 
         if ($table['status'] == 1 && $table['hidden'] != 1) {
+
             $be_page = new rex_be_page_main('yform_tables', $table['table_name'], $table['name']);
             $be_page->setHref('index.php?page=yform/manager/data_edit&table_name=' . $table['table_name']);
             $be_page->setIcon('rex-icon rex-icon-module');
             $be_page->setRequiredPermissions([$table_perm]);
+            $be_page->setPrio($prio);
             if (rex_request('page','string') == 'yform/manager/data_edit' && rex_request('table_name', 'string') == $table['table_name']) {
                 $be_page->setIsActive();
 
@@ -36,6 +39,7 @@ if (rex::isBackend() && rex::getUser()) {
 
             $pages[] = $be_page;
 
+            $prio++;
             // TODO rechte noch verfügbar machen
 
         }
