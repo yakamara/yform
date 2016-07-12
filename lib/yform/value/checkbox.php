25e5627c48e2b90e8293c61061ddbbcf685f471d
Alexander Walther <github.com@alexplus.de>
2016-07-12 11:00:29 +0200

    Revert "aktuelle checkbox.php aus yakamara/YForm"
    This reverts commit 21ffa14f374035fe98ec56426c029730af05a44a.

--- lib/yform/value/checkbox.php
+++ lib/yform/value/checkbox.php
@@ -46,7 +46,7 @@ class rex_yform_value_checkbox extends rex_yform_value_abstract
         $this->params['form_output'][$this->getId()] = $this->parse('value.checkbox.tpl.php', array('value' => $v));
 
         ## set values
-        $this->params['value_pool']['email'][$this->getName()] = $this->getValue();
+        $this->params['value_pool']['email'][$this->getName()] = stripslashes($this->getValue());
         if ($this->getElement(5) != 'no_db') {
             $this->params['value_pool']['sql'][$this->getName()] = $this->getValue();
         }
@@ -63,11 +63,11 @@ class rex_yform_value_checkbox extends rex_yform_value_abstract
             'type' => 'value',
             'name' => 'checkbox',
             'values' => array(
-                'name'    => array( 'type' => 'name', 'label' => 'Name' ),
-                'label'   => array( 'type' => 'text', 'label' => 'Bezeichnung'),
-                'values'  => array( 'type' => 'text', 'label' => 'Werte (0,1) (nicht angeklickt,angeklickt)', 'default' => '0,1'),
-                'default' => array( 'type' => 'boolean', 'label' => 'Defaultstatus', 'default' => 0),
-                'no_db'   => array( 'type' => 'no_db', 'label' => 'Datenbank', 'default' => 0),
+                'name'    => array( 'type' => 'name', 'label' => rex_i18n::msg("yform_values_defaults_name")),
+                'label'   => array( 'type' => 'text', 'label' => rex_i18n::msg("yform_values_defaults_label")),
+                'values'  => array( 'type' => 'text', 'label' => rex_i18n::msg("yform_values_checkbox_values"), 'default' => '0,1'),
+                'default' => array( 'type' => 'boolean', 'label' => rex_i18n::msg("yform_values_checkbox_default"), 'default' => 0),
+                'no_db'   => array( 'type' => 'no_db', 'label' => rex_i18n::msg("yform_values_defaults_table"), 'default' => 0),
             ),
             'description' => rex_i18n::msg("yform_values_checkbox_description"),
             'dbtype' => 'varchar(255)',
