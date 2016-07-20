cd807543ad78085202f1c2b34ad27a6cf148dcbe
Gregor Harlan <mail@gh01.de>
2016-07-20 10:09:22 +0200

    Autoload-Cache löschen bei Update
    löst eventuell erstmal #119

--- update.php
+++ update.php
@@ -12,3 +12,5 @@ if ($this->getPlugin('manager')->isInstalled()) {
     // use path relative to __DIR__ to get correct path in update temp dir
     $this->getPlugin('manager')->includeFile(__DIR__.'/plugins/manager/install.php');
 }
+
+rex_autoload::removeCache();
