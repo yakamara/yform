efa236f4de9dc2e872a9759cdf17c7eabf3162c8
Jan Kristinus <jan.kristinus@yakamara.de>
2016-08-31 12:15:49 +0200

    HTML Value um Label ergänzt, damit es beim Manager funktioniert

--- lib/yform/value/html.php
+++ lib/yform/value/html.php
@@ -11,12 +11,12 @@ class rex_yform_value_html extends rex_yform_value_abstract
 
     function enterObject()
     {
-        $this->params['form_output'][$this->getId()] = $this->getElement(2);
+        $this->params['form_output'][$this->getId()] = $this->getElement('html');
     }
 
     function getDescription()
     {
-        return htmlspecialchars('html -> Beispiel: html|name|<div class="block"></div>');
+        return htmlspecialchars('html -> Beispiel: html|label|<div class="block"></div>');
     }
 
     function getDefinitions()
@@ -27,6 +27,7 @@ class rex_yform_value_html extends rex_yform_value_abstract
             'name' => 'html',
             'values' => array(
                 'name' => array( 'type' => 'name',   'label' => rex_i18n::msg("yform_values_defaults_name")),
+                'label' => array( 'type' => 'text',    'label' => rex_i18n::msg("yform_values_defaults_label")),
                 'html' => array( 'type' => 'textarea',    'label' => rex_i18n::msg("yform_values_html_HTML")),
             ),
             'description' => rex_i18n::msg("yform_values_html_description"),
