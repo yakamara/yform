7f13015cc9e2521170233c0d1eddf749bfec5c69
tbaddade <thomas@redaxo.org>
2016-10-05 15:08:01 +0200

    REDAXO Style verwenden, close #152

--- plugins/tools/boot.php
+++ plugins/tools/boot.php
@@ -18,11 +18,25 @@ if (rex::isBackend() && rex::getUser()) {
     rex_view::addJsFile($this->getAssetsUrl('select2/dist/js/select2.min.js'));
     rex_view::addCssFile($this->getAssetsUrl('select2/dist/css/select2.min.css'));
 
-    rex_view::addCssFile($this->getAssetsUrl('select2-bootstrap-theme/dist/select2-bootstrap.min.css'));
+    //rex_view::addCssFile($this->getAssetsUrl('select2-bootstrap-theme/dist/select2-bootstrap.min.css'));
 
     rex_view::addJsFile($this->getAssetsUrl('inputmask/dist/min/jquery.inputmask.bundle.min.js'));
 
-    rex_view::addCssFile($this->getAssetsUrl('tools.css'));
     rex_view::addJsFile($this->getAssetsUrl('tools.js'));
+
+
+    rex_extension::register('PACKAGES_INCLUDED', function () {
+        if ($this->getProperty('compile')) {
+            $compiler = new rex_scss_compiler();
+
+            $scss_files = [$this->getPath('scss/master.scss')];
+            $compiler->setRootDir($this->getPath('scss/'));
+            $compiler->setScssFile($scss_files);
+            $compiler->setCssFile($this->getPath('assets/css/styles.css'));
+            $compiler->compile();
+            rex_file::copy($this->getPath('assets/css/styles.css'), $this->getAssetsPath('css/styles.css'));
+        }
+    });
+    rex_view::addCssFile($this->getAssetsUrl('css/styles.css'));
 }
 
