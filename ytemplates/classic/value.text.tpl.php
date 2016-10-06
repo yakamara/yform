1fc7b37df22f1de81bade696bb604bdbd42e8573
tbaddade <thomas@redaxo.org>
2016-10-06 11:52:49 +0200

    Classic Theme Anpassungen

--- ytemplates/classic/value.text.tpl.php
+++ ytemplates/classic/value.text.tpl.php
@@ -1,9 +1,18 @@
 <?php
-    $type = isset($type) ? $type : 'text';
-    $class = $type == 'text' ? '' : $type . ' ';
-    $value = $this->getValue();
+$type = isset($type) ? $type : 'text';
+$class = $type == 'text' ? '' : $type . ' ';
+$value = $this->getValue();
+
+$attributes = [
+    "class" => trim('text ' . $class . ' ' . $this->getWarningClass()),
+    "name" => $this->getFieldName(),
+    "type" => $type,
+    "id" => $this->getFieldId(),
+    "value" => $value
+];
+$attributes = $this->getAttributeElements($attributes, ['placeholder', 'autocomplete', 'pattern', 'required', 'disabled', 'readonly']);
 ?>
 <p class="formtext <?php echo $class ? 'form' . $class : '' ?>formlabel-<?php echo $this->getName() ?>" id="<?php echo $this->getHTMLId() ?>">
-    <label class="text <?php echo $class, ' ', $this->getWarningClass() ?>" for="<?php echo $this->getFieldId() ?>" ><?php echo $this->getLabel() ?></label>
-    <input type="<?php echo $type ?>" class="text <?php echo $class, ' ', $this->getElement(5), ' ', $this->getWarningClass() ?>" name="<?php echo $this->getFieldName() ?>" id="<?php echo $this->getFieldId() ?>" value="<?php echo htmlspecialchars($value) ?>"<?php echo $this->getAttributeElements('placeholder'), $this->getAttributeElements('autocomplete'), $this->getAttributeElements('pattern'), $this->getAttributeElements('required', true), $this->getAttributeElements('disabled', true), $this->getAttributeElements('readonly', true) ?> />
+    <label class="<?php echo trim('text ' . $class . ' ' . $this->getWarningClass()) ?>" for="<?php echo $this->getFieldId() ?>" ><?php echo $this->getLabel() ?></label>
+    <input <?= implode(' ', $attributes) ?> />
 </p>
--- ytemplates/classic/value.textarea.tpl.php
+++ ytemplates/classic/value.textarea.tpl.php
@@ -1,4 +1,20 @@
+<?php
+
+$rows = $this->getElement('rows');
+if (!$rows) {
+    $rows = 10;
+}
+
+$attributes = [
+    "class" => trim('textarea ' . $this->getWarningClass()),
+    "name" => $this->getFieldName(),
+    "id" => $this->getFieldId(),
+    "rows" => $rows
+];
+
+$attributes = $this->getAttributeElements($attributes, ['placeholder', 'pattern', 'required', 'disabled', 'readonly']);
+?>
 <p class="formtextarea" id="<?php echo $this->getHTMLId() ?>">
     <label class="<?php echo trim('textarea ' . $this->getWarningClass()) ?>" for="<?php echo $this->getFieldId() ?>" ><?php echo $this->getLabel() ?></label>
-    <textarea class="<?php echo trim('textarea ' . $this->getElement(5) . ' ' . $this->getWarningClass()) ?>" name="<?php echo $this->getFieldName() ?>" id="<?php echo $this->getFieldId() ?>" cols="80" rows="10" <?php echo $this->getAttributeElements('placeholder'), $this->getAttributeElements('pattern'), $this->getAttributeElements('required', true), $this->getAttributeElements('disabled', true), $this->getAttributeElements('readonly', true) ?>><?php echo htmlspecialchars($this->getValue()) ?></textarea>
+    <textarea <?= implode(' ', $attributes) ?>><?php echo htmlspecialchars($this->getValue()) ?></textarea>
 </p>
