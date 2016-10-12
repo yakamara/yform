391e39969d560601deef7e38c4fd9ae74225302d
Jan Kristinus <jan.kristinus@yakamara.de>
2016-10-12 10:57:47 +0200

    Permissions Tippfehler entfernt fixed #180

--- plugins/manager/lib/perm.php
+++ plugins/manager/lib/perm.php
@@ -14,9 +14,9 @@ class rex_yform_manager_table_perm extends rex_complex_perm
     public static function getFieldParams()
     {
         return [
-            'label' => rex_i18n::msg('yform_mamager_table'),
-            'all_label' => rex_i18n::msg('yform_mamager_tables'),
-            'sql_options' => 'select name, table_name as id from ' . rex::getTablePrefix() . 'yform_table where status = 1 order by prio',
+            'label' => rex_i18n::msg('yform_manager_table'),
+            'all_label' => rex_i18n::msg('yform_manager_tables'),
+            'sql_options' => 'select concat(`name`," [", `table_name`,"]") as name, table_name as id from ' . rex::getTablePrefix() . 'yform_table where status = 1 order by prio',
         ];
     }
 
