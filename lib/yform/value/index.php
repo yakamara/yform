7d39509cb5f6dfa616e4ee7371b3b733a673e827
Gregor Harlan <mail@gh01.de>
2016-12-06 16:03:34 +0100

    Index-Feld: auch Multiple-Relationen ermöglichen
    #212

--- lib/yform/value/index.php
+++ lib/yform/value/index.php
@@ -102,7 +102,7 @@ class rex_yform_value_index extends rex_yform_value_abstract
         foreach ($relations as $name => $sub) {
             $relation = $table->getRelation($name);
 
-            if (!$relation || !in_array($relation->getElement('type'), [0, 2]) || empty($this->params['value_pool']['sql'][$name])) {
+            if (!$relation || 4 != $relation->getElement('type') && !$relation->getElement('relation_table') && empty($this->params['value_pool']['sql'][$name])) {
                 continue;
             }
 
@@ -110,45 +110,135 @@ class rex_yform_value_index extends rex_yform_value_abstract
             $joins = [];
             $maxIndex = 0;
 
-            $addFieldsAndJoins = function (array $columns, rex_yform_manager_table $table, $index) use (&$addFieldsAndJoins, &$fields, &$joins, &$maxIndex, $sql) {
+            $addJoin = function ($table, $index, $name, $type) use ($sql, &$joins, &$maxIndex) {
+                switch ($type) {
+                    case 0:
+                    case 2:
+                        $format = 'LEFT JOIN %s t%d ON t%2$d.id = t%d.%s';
+                        break;
+                    case 1:
+                    case 3:
+                        $format = 'LEFT JOIN %s t%d ON FIND_IN_SET(t%2$d.id, t%d.%s)';
+                        break;
+                    case 4:
+                        $format = 'LEFT JOIN %s t%d ON t%2$d.%4$s = t%d.id';
+                        break;
+                    default:
+                        throw new LogicException(sprintf('Unknown relation type "%s"', $type));
+                }
+
+                $nextIndex = ++$maxIndex;
+                $joins[] = sprintf(
+                    $format,
+                    $sql->escapeIdentifier($table),
+                    $nextIndex,
+                    $index,
+                    $sql->escapeIdentifier($name)
+                );
+
+                return $nextIndex;
+            };
+
+            $addFieldsAndJoins = function (array $columns, rex_yform_manager_field $relation, $index) use (&$addFieldsAndJoins, $addJoin, &$fields, &$joins, &$maxIndex, $sql) {
+                $table = rex_yform_manager_table::get($relation->getElement('table'));
+
+                $fieldFormat = 't%d.%s';
+                if ($relation->getElement('relation_table') || in_array($relation->getElement('type'), [1, 3, 4])) {
+                    $fieldFormat = 'GROUP_CONCAT('.$fieldFormat.' SEPARATOR " ")';
+                }
+
                 foreach ($columns as $name => $sub) {
                     if (!$sub) {
-                        $fields[] = 't'.$index.'.'.$sql->escapeIdentifier($name);
+                        $fields[] = sprintf($fieldFormat, $index, $sql->escapeIdentifier($name));
                         continue;
                     }
 
                     $relation = $table->getRelation($name);
 
-                    if (!$relation || !in_array($relation->getElement('type'), [0, 2])) {
+                    if (!$relation) {
                         continue;
                     }
 
-                    $nextIndex = ++$maxIndex;
-                    $joins[] = sprintf(
-                        'LEFT JOIN %s t%d ON t%2$d.id = t%d.%s',
-                        $sql->escapeIdentifier($relation['table']),
-                        $nextIndex,
-                        $index,
-                        $sql->escapeIdentifier($name)
-                    );
+                    $currentIndex = $index;
 
-                    $addFieldsAndJoins($sub, rex_yform_manager_table::get($relation['table']), $nextIndex);
+                    if ($relation->getElement('relation_table')) {
+                        try {
+                            $columns = $table->getRelationTableColumns($name);
+                        } catch (RuntimeException $e) {
+                            continue;
+                        }
+
+                        $relationTable = rex_yform_manager_table::get($relation->getElement('relation_table'));
+                        $name = $columns['target'];
+                        $relation = $relationTable->getValueField($name);
+                        $relation['relation_table'] = true;
+
+                        $currentIndex = $addJoin($relationTable->getTableName(), $currentIndex, $columns['source'], 4);
+
+                    }
+
+                    if (4 == $relation->getElement('type')) {
+                        $name = $relation->getElement('field');
+                    }
+
+                    $currentIndex = $addJoin($relation->getElement('table'), $currentIndex, $name, $relation->getElement('type'));
+
+                    $addFieldsAndJoins($sub, $relation, $currentIndex);
                 }
             };
 
-            $addFieldsAndJoins($sub, rex_yform_manager_table::get($relation['table']), $maxIndex);
+            $fromTable = $relation->getElement('table');
+            $type = $relation->getElement('type');
+
+            if ($relation->getElement('relation_table')) {
+                try {
+                    $columns = $table->getRelationTableColumns($name);
+                } catch (RuntimeException $e) {
+                    continue;
+                }
+
+                $fromTable = $relation->getElement('relation_table');
+                $relationTable = rex_yform_manager_table::get($fromTable);
+                $relation = $relationTable->getValueField($columns['target']);
+                $relation['relation_table'] = true;
+                $type = 'relation_table';
+
+                $maxIndex = $addJoin($relation->getElement('table'), $maxIndex, $columns['target'], 0);
+            }
+
+            $addFieldsAndJoins($sub, $relation, $maxIndex);
 
             if (!$fields) {
                 continue;
             }
 
-            $data = $sql->getArray(sprintf(
-                'SELECT %s FROM %s t0 %s WHERE t0.id = %d LIMIT 1',
+            $query = sprintf(
+                'SELECT %s FROM %s t0 %s WHERE ',
                 implode(', ', $fields),
-                $sql->escapeIdentifier($relation['table']),
-                implode(' ', $joins),
-                $this->params['value_pool']['sql'][$name]
-            ));
+                $sql->escapeIdentifier($fromTable),
+                implode(' ', $joins)
+            );
+
+            switch ($type) {
+                case 'relation_table':
+                    $query .= sprintf('t0.%s = %d', $sql->escapeIdentifier($columns['source']), $this->params['main_id']);
+                    break;
+                case 0:
+                case 2:
+                    $query .= sprintf('t0.id = %d', $this->params['value_pool']['sql'][$name]);
+                    break;
+                case 1:
+                case 3:
+                    $query .= sprintf('FIND_IN_SET(t0.id, %s)', $sql->escape($this->params['value_pool']['sql'][$name]));
+                    break;
+                case 4:
+                    $query .= sprintf('t0.%s = %d', $sql->escapeIdentifier($relation->getElement('field')), $this->params['main_id']);
+                    break;
+                default:
+                    throw new LogicException(sprintf('Unknown relation type "%s"', $type));
+            }
+
+            $data = $sql->getArray($query.' LIMIT 1');
 
             if (!isset($data[0])) {
                 continue;
