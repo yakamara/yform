1091c731f5f008f669e9552677fa424b335b720f
Jan Kristinus <jan.kristinus@yakamara.de>
2017-01-12 23:26:39 +0100

    manager: Import Tableset korrigiert.

--- plugins/manager/pages/tableset_import.php
+++ plugins/manager/pages/tableset_import.php
@@ -13,39 +13,38 @@ $page = rex_request('page', 'string', '');
 $yform = new rex_yform;
 $yform->setHiddenField('page', $page);
 $yform->setObjectparams('real_field_names',true);
-$yform->setObjectparams('hide_top_warning_messages', true);
-$yform->setObjectparams('form_showformafterupdate',1);
-$yform->setValueField('upload', array(
-    'name'     => 'importfile',
-    'label'    => rex_i18n::msg('yform_manager_tableset_import_jsonimportfile'),
-    'max_size' => '1000', // max size in kb or range 100,500
-    'types'    => '.json', // allowed extensions ".gif,.png"
-    'required' => 1,
-    'messages' => array(
+$yform->setObjectparams('hide_top_warning_messages', false);
+$yform->setValueField('upload', [
+    'name' => 'importfile',
+    'label' => rex_i18n::msg('yform_manager_tableset_import_jsonimportfile'),
+    'sizes' => '1000',
+    'types' => '.json',
+    'required' => true,
+    'messages' => [
         rex_i18n::msg('yform_manager_table_importset_warning_min'),
         rex_i18n::msg('yform_manager_table_importset_warning_max'),
         rex_i18n::msg('yform_manager_table_importset_warning_type'),
         rex_i18n::msg('yform_manager_table_importset_warning_selectfile')
-    ),
-    'modus'    => 'no_save',
-    'no_db'    => 'no_db'
-));
+    ]
+]);
 
 $form = $yform->getForm();
 
 if ($yform->objparams['actions_executed']) {
 
     try {
+        $filder = $yform->objparams['value_pool']['files']['importfile'][2];
 
-        $content = file_get_contents(rex_path::addonData('yform','uploads/'.$yform->objparams['value_pool']['email']['importfile']));
-        rex_yform_manager_table_api::importTablesets($content);
+        $content = file_get_contents($filder);
+        echo rex_yform_manager_table_api::importTablesets($content);
         echo rex_view::success(rex_i18n::msg('yform_manager_tableset_import_success'));
 
     } catch (Exception $e) {
-        echo rex_view::warning(rex_i18n::msg('yform_manager_tableset_import_failed', '', $e->getMessage()));
+        echo rex_view::warning(rex_i18n::msg('yform_manager_tableset_import_failed', $e->getMessage()));
 
     }
-    rex_file::delete(rex_path::addonData('yform','uploads/'.$yform->objparams['value_pool']['email']['importfile']));
+
+    rex_file::delete($filder);
 
 } else if ($yform->objparams['send']) {
 
@@ -53,11 +52,13 @@ if ($yform->objparams['actions_executed']) {
 
 }
 
-$fragment = new rex_fragment();
-$fragment->setVar('class', 'edit', false);
-$fragment->setVar('title', rex_i18n::msg('yform_manager_tableset_import'));
-$fragment->setVar('body', $form, false);
+if ($form != "") {
+    $fragment = new rex_fragment();
+    $fragment->setVar('class', 'edit', false);
+    $fragment->setVar('title', rex_i18n::msg('yform_manager_tableset_import'));
+    $fragment->setVar('body', $form, false);
 // $fragment->setVar('buttons', $buttons, false);
-$form = $fragment->parse('core/page/section.php');
+    $form = $fragment->parse('core/page/section.php');
 
-echo $form;
+    echo $form;
+}
