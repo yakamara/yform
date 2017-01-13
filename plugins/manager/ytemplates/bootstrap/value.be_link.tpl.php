616ccff55978d0739cec7a9c5d2eedf44a9f68de
Jan Kristinus <jankristinus@Jans-MacBook-Pro-2017.local>
2017-01-13 12:55:06 +0100

    be_link um mehrfachauswahl ergänzt fixed #193

--- plugins/manager/ytemplates/bootstrap/value.be_link.tpl.php
+++ plugins/manager/ytemplates/bootstrap/value.be_link.tpl.php
@@ -4,20 +4,24 @@ $buttonId = $counter;
 $categoryId = 0;
 $name = $this->getFieldName();
 $value = htmlspecialchars($this->getValue());
-$widget = rex_var_link::getWidget($buttonId, $name, $value, ['category' => $categoryId]);
+
+if ($this->getElement('multiple') == 1) {
+    $widget = rex_var_linklist::getWidget($buttonId, $name, $value, []);
+} else {
+    $widget = rex_var_link::getWidget($buttonId, $name, $value, []);
+}
 
 $class_group = trim('form-group yform-element ' . $this->getHTMLClass() . ' ' . $this->getWarningClass());
 
-$notice = array();
-if ($this->getElement('notice') != "") {
+$notice = [];
+if ($this->getElement('notice') != '') {
     $notice[] = $this->getElement('notice');
 }
 if (isset($this->params['warning_messages'][$this->getId()]) && !$this->params['hide_field_warning_messages']) {
-    $notice[] =  '<span class="text-warning">' . rex_i18n::translate($this->params['warning_messages'][$this->getId()], null, false) . '</span>'; //    var_dump();
+    $notice[] = '<span class="text-warning">' . rex_i18n::translate($this->params['warning_messages'][$this->getId()], null, false) . '</span>'; //    var_dump();
 }
 if (count($notice) > 0) {
-    $notice = '<p class="help-block">' . implode("<br />", $notice) . '</p>';
-
+    $notice = '<p class="help-block">' . implode('<br />', $notice) . '</p>';
 } else {
     $notice = '';
 }
