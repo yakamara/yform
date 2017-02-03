cd81498ce9ac55bfd89fd1420669070d8f972f73
Jan Kristinus <jan.kristinus@yakamara.de>
2017-02-03 17:37:07 +0100

    generate key mit definitions ergänzt und ansicht im backend

--- lib/yform/value/generate_key.php
+++ lib/yform/value/generate_key.php
@@ -1,7 +1,8 @@
 <?php
 
 /**
- * yform
+ * yform.
+ *
  * @author jan.kristinus[at]redaxo[dot]org Jan Kristinus
  * @author <a href="http://www.yakamara.de">www.yakamara.de</a>
  */
@@ -9,18 +10,66 @@
 class rex_yform_value_generate_key extends rex_yform_value_abstract
 {
 
-    function enterObject()
+    function preValidateAction()
     {
-        $this->setValue(md5($this->params['form_name'] . substr(md5(microtime()), 0, 6)));
-        $this->params['form_output'][$this->getId()] = '';
-        $this->params['value_pool']['email'][$this->getName()] = $this->getValue();
-        if ($this->getElement(2) != 'no_db') {
-            $this->params['value_pool']['sql'][$this->getName()] = $this->getValue();
+        $generated_key = md5($this->params['form_name'].uniqid(rand(), true));
+
+        if ($this->getElement('only_empty') != 1) {
+            // wird immer neu gesetzt
+            $this->setValue($generated_key);
+
+        } elseif ($this->getValue() != "") {
+            // wenn Wert vorhanden ist direkt zurück
+
+        } elseif (isset($this->params['sql_object']) && $this->params['sql_object']->getValue($this->getName()) != "") {
+            // sql object vorhanden und Wert gesetzt ?
+
+        } else {
+            $this->setValue($generated_key);
+
         }
+
     }
 
-    function getDescription()
+
+    public function enterObject()
     {
-        return 'generate_key|name|[no_db]';
+        if ($this->needsOutput() && $this->getElement('show_value') == 1) {
+            $this->params['form_output'][$this->getId()] = $this->parse('value.showvalue.tpl.php');
+        }
+
+
+        $this->params['value_pool']['email'][$this->getName()] = $this->getValue();
+        if ($this->getValue() && $this->getElement('no_db') != 'no_db') {
+            $this->params['value_pool']['sql'][$this->getName()] = $this->getValue();
+        }
+
     }
+
+    public function getDescription()
+    {
+        return 'generate_key|name|label|[no_db][0-wird immer neu gesetzt,1-nur wenn leer]';
+    }
+
+    function getDefinitions()
+    {
+
+        return array(
+            'type' => 'value',
+            'name' => 'generate_key',
+            'values' => array(
+                'name'  =>  array( 'type' => 'name',   'label' => rex_i18n::msg("yform_values_defaults_name")),
+                'label' => array( 'type' => 'text',    'label' => rex_i18n::msg("yform_values_defaults_label")),
+                'only_empty' => array( 'type' => 'select',  'label' => rex_i18n::msg("yform_values_datestamp_only_empty"), 'default' => '0', 'options' => 'immer=0,nur wenn leer=1' ),
+                'show_value' => array( 'type' => 'checkbox',  'label' => rex_i18n::msg("yform_values_defaults_showvalue"), 'default' => '0', 'options' => '0,1' ),
+
+            ),
+            'description' => rex_i18n::msg("yform_values_datestamp_description"),
+            'dbtype' => 'varchar(255)',
+            'multi_edit' => 'always',
+        );
+
+
+    }
+
 }
