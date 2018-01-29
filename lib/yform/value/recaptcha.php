35e4f77b2a091a326450f2ccc236d34a44b9b9fc
Alexander Walther <github.com@alexplus.de>
2018-01-29 10:46:48 +0100

    recaptcha (#415)
    closes #331
    von @elpschulzo
    
    liefert ein classic-template mit. bootstrap-template evtl. noch
    verbesserungswürdig.
    
    (von mir ungetestet)

---
+++ lib/yform/value/recaptcha.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * XForm
+ * @author p.schulze@bitshifters.de
+ * @author <a href="http://www.bitshifters.de">www.bitshifters.de</a>
+ */
+
+class rex_yform_value_recaptcha extends rex_yform_value_abstract
+{
+	public function enterObject()
+	{
+		if ($this->getElement(2) == '') {
+			return;
+		}
+		
+		$this->params['form_output'][$this->getId()] = $this->parse('value.recaptcha.tpl.php');
+	}
+	
+	public function getDescription()
+	{
+		return 'recaptcha|name|public_key|load_script(bool)|[no_db]<br />Beispiel: recaptcha|botcheck|XLKJHD1233-D|true|[no_db]';
+	}
+
+}
---
+++ ytemplates/bootstrap/value.recaptcha.tpl.php
@@ -0,0 +1,7 @@
+<?php
+if($this->getElement(3)) {
+	echo '<script type="text/javascript" src="https://www.google.com/recaptcha/api.js"></script>';
+}
+
+?>
+<div id="<?php echo $this->getHTMLId() ?>" class="g-recaptcha <?php echo $this->getWarningClass(); ?>" data-sitekey="<?php echo $this->getElement(2); ?>"></div>
