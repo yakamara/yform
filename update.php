afc8a383093ca74dff8601f926ecb0e7f6bd8b20
Gregor Harlan <mail@gh01.de>
2018-03-17 13:18:14 +0100

    Data-Ordner (fonts) nicht mehr kopieren

--- update.php
+++ update.php
@@ -4,9 +4,7 @@
  * @var rex_addon $this
  */
 
-rex_extension::register('OUTPUT_FILTER', function () {
-    rex_dir::copy($this->getPath('data'), $this->getDataPath());
-});
+rex_dir::delete($this->getDataPath('fonts'));
 
 foreach ($this->getInstalledPlugins() as $plugin) {
     // use path relative to __DIR__ to get correct path in update temp dir
