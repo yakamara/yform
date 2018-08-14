cac3e1e234a6634f70b458c369b8365b3bcc33e1
Jan Kristinus <jan.kristinus@yakamara.de>
2018-08-14 14:55:57 +0200

    html value info um label ergänzt #486 fixed

--- lib/yform/value/html.php
+++ lib/yform/value/html.php
@@ -28,7 +28,7 @@ class rex_yform_value_html extends rex_yform_value_abstract
 
     public function getDescription()
     {
-        return htmlspecialchars('html|name|<div class="block"></div>');
+        return htmlspecialchars('html|name|label|<div class="block"></div>');
     }
 
     public function getDefinitions()
