a0aeddf61afd97229d1155b9bb8c7c13403e7d10
Jan Kristinus <jan.kristinus@yakamara.de>
2018-09-07 11:11:34 +0200

    Float Feld als deprecated markiert

--- lib/yform/value/float.php
+++ lib/yform/value/float.php
@@ -46,6 +46,7 @@ class rex_yform_value_float extends rex_yform_value_abstract
                 'notice' => ['type' => 'text',    'label' => rex_i18n::msg('yform_values_defaults_notice')],
             ],
             'description' => rex_i18n::msg('yform_values_float_description'),
+            'deprecated' => rex_i18n::msg('yform_values_deprecated_float'),
             'db_type' => ['varchar(191)'],
         ];
     }
