ad632c7ee091aff406545fb47ccbafe32d97d178
Tobias Krais <tk@design-to-use.de>
2018-11-30 15:48:17 +0100

    Schreibfehler korrigiert

--- lib/yform/value/date.php
+++ lib/yform/value/date.php
@@ -165,7 +165,7 @@ class rex_yform_value_date extends rex_yform_value_abstract
 
     public function getDescription()
     {
-        return 'date|name|label| jahrstart | [jahrsende/+5 ]| [Anzeigeformat YYYY-MM-DD] | [1/Aktuelles Datum voreingestellt] | [no_db] ';
+        return 'date|name|label| jahrstart | [jahrende/+5 ]| [Anzeigeformat YYYY-MM-DD] | [1/Aktuelles Datum voreingestellt] | [no_db] ';
     }
 
     public function getDefinitions()
