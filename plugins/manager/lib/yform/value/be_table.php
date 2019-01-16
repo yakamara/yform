e1ca94841fa6114b7f2043744375e2d8de8f3cc8
Alex Platter <alex.platter@gmail.com>
2019-01-16 17:42:17 +0100

    be_manager_relation mit be_table widget erweiter
    man kann somit neben select, popup und inline nun auch be_table als be_manager_relation input verwenden.
    VORTEIL: wenn man eine N-M Tabelle hat, welche neben der Relation noch weiter Felder beinhaltet kann man die damit einpflegen
    
    Zusatz: be_table wurde noch etwas verbessert sodass man es nun auch mobil verwenden kann

--- plugins/manager/lib/yform/value/be_table.php
+++ plugins/manager/lib/yform/value/be_table.php
@@ -8,6 +8,8 @@
  */
 class rex_yform_value_be_table extends rex_yform_value_abstract
 {
+    protected $fieldData = [];
+
     public function preValidateAction()
     {
         // bc service for Version < 1.1
@@ -49,7 +51,7 @@ class rex_yform_value_be_table extends rex_yform_value_abstract
     public function enterObject()
     {
         $this->params['value_pool']['email'][$this->getName()] = $this->getValue();
-        if ($this->getElement('no_db') != 'no_db') {
+        if ($this->getElement('no_db') != 1) {
             $this->params['value_pool']['sql'][$this->getName()] = $this->getValue();
         }
 
@@ -67,7 +69,9 @@ class rex_yform_value_be_table extends rex_yform_value_abstract
         $columns = [];
         $objs = [];
         $columnIndex = [];
-        
+
+        $this->fieldData = $data;
+
         $yfparams = \rex_yform::factory()->objparams;
         $yfparams['this'] = \rex_yform::factory();
 
@@ -76,7 +80,8 @@ class rex_yform_value_be_table extends rex_yform_value_abstract
          */
 
         foreach ($_columns as $index => $col) {
-            $values = explode('|', trim(trim(rex_yform::unhtmlentities($col)), '|'));
+            // Use ;; for separating choice columns instead of ,
+            $values = explode('|', trim(trim(str_replace(';;', ',', rex_yform::unhtmlentities($col))), '|'));
             if (count($values) == 1) {
                 $values = ['text', 'text_'. $index, $values[0]];
             }
@@ -97,8 +102,9 @@ class rex_yform_value_be_table extends rex_yform_value_abstract
 
                 foreach($data as $rowCount => $row) {
                     $obj = clone $field;
+                    $rdata = array_values($data[$rowCount]);
                     $obj->setName($name.$rowCount.$index);
-                    $obj->setValue($data[$rowCount][$index]);
+                    $obj->setValue($rdata[$index]);
                     $objs[]= $obj;
                 }
 
@@ -130,9 +136,22 @@ class rex_yform_value_be_table extends rex_yform_value_abstract
         }
 
         $this->params['form_output'][$this->getId()] = $this->parse('value.be_table.tpl.php', compact('columns', 'data'));
+
+        if ($this->getParam('send')) {
+            $this->setValue(json_encode($this->fieldData));
+
+            if ($this->getElement('no_db') != 1) {
+                $this->params['value_pool']['sql'][$this->getName()] = $this->getValue();
+            }
+            $this->params['value_pool']['email'][$this->getName()] = $this->getValue();
+        }
     }
 
-    public function getDefinitions()
+    public function setFieldData($index, $key, $value) {
+        $this->fieldData[$index][$key] = $value;
+    }
+
+    public function getDefinitions($values = [])
     {
         return [
             'type'        => 'value',
