a781aac3cdce255b85820c62f38b3da720e53e0d
8i11y <48041487+8i11y@users.noreply.github.com>
2019-03-04 15:45:14 +0100

    Correct value.checkbox.tpl.php in classic-Template (#683)
    Danke @8i11y

--- ytemplates/classic/value.checkbox.tpl.php
+++ ytemplates/classic/value.checkbox.tpl.php
@@ -1,5 +1,5 @@
 <?php $value = isset($value) ? $value : 1 ?>
 <p class="formcheckbox formlabel-<?php echo $this->getName() ?>" id="<?php echo $this->getHTMLId() ?>">
-    <input type="checkbox" class="checkbox<?php echo $this->getWarningClass() ?>" name="<?php echo $this->getFieldName() ?>" id="<?php echo $this->getFieldId() ?>" value="<?php echo $value ?>"<?php echo $this->getValue() == $value ? ' checked="checked"' : '' ?> />
+    <input type="checkbox" class="checkbox<?php echo $this->getWarningClass() ?>" name="<?php echo $this->getFieldName() ?>" id="<?php echo $this->getFieldId() ?>" value="1"<?php echo $this->getValue() == 1 ? ' checked="checked"' : '' ?> />
     <label class="checkbox <?php echo $this->getWarningClass() ?>" for="<?php echo $this->getFieldId() ?>" ><?php echo $this->getLabel() ?></label>
 </p>
