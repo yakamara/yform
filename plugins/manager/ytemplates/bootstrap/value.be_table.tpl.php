22cddb50660bff45b49b3068cc2e886853908ef9
Alex Platter <alex.platter@gmail.com>
2019-04-03 10:19:56 +0200

    be_table Bugfix (#705)
    * kreatif merge

--- plugins/manager/ytemplates/bootstrap/value.be_table.tpl.php
+++ plugins/manager/ytemplates/bootstrap/value.be_table.tpl.php
@@ -2,7 +2,7 @@
 $class_group = trim('form-group ' . $this->getHTMLClass() . ' ' . $this->getWarningClass());
 
 $data_index = 0;
-$notice = [];
+$notice     = [];
 if ($this->getElement('notice') != '') {
     $notice[] = rex_i18n::translate($this->getElement('notice'), false);
 }
@@ -15,6 +15,9 @@ if (count($notice) > 0) {
     $notice = '';
 }
 
+$ytemplates = $this->params['this']->getObjectparams('form_ytemplate');
+$main_id    = $this->params['this']->getObjectparams('main_id');
+
 ?>
 <div class="<?= $class_group ?>" id="<?= $this->getHTMLId() ?>">
     <label class="control-label" for="<?php echo $this->getFieldId() ?>"><?php echo $this->getLabel() ?></label>
@@ -22,42 +25,53 @@ if (count($notice) > 0) {
         <thead>
         <tr>
             <?php foreach ($columns as $column): ?>
-                <th><?php echo htmlspecialchars($column['label']) ?></th>
+                <th class="type-<?= $column['field']->getElement(0) ?>"><?php echo htmlspecialchars($column['label']) ?></th>
             <?php endforeach ?>
-            <th class="rex-table-action"><a class="btn btn-xs btn-default" id="<?= $this->getHTMLId() ?>-add-row" href="javascript:void(0);"><i class="rex-icon rex-icon-add"></i> <?php echo rex_i18n::msg('yform_add_row') ?></a></th>
+            <th class="rex-table-action"><a class="btn btn-xs btn-primary" id="<?= $this->getHTMLId() ?>-add-row" href="javascript:void(0);"><i class="rex-icon rex-icon-add"></i> <?php echo rex_i18n::msg('yform_add_row') ?></a></th>
         </tr>
         </thead>
         <tbody>
         <?php foreach ($data as $data_index => $row): ?>
             <tr>
                 <?php foreach ($columns as $i => $column): ?>
-                    <td class="be-value-input">
-                        <?php
-                            $field = $column['field'];
-                            $field->params['this']->setObjectparams('form_name', $this->getId() .'.'. $i);
-                            $field->params['form_name'] = $field->getName();
-                            $field->params['form_label_type'] = 'html';
-                            $field->params['send'] = false;
-                            $field->setValue($row[$i] ?: '');
-                            $field->setId($data_index);
-                            $field->enterObject();
-                            echo $field->params['form_output'][$field->getId()]
-                        ?>
-                    </td>
+                    <?php
+                    $rowData = array_values($row);
+                    $field = $column['field'];
+                    $field->params['form_output'] = [];
+                    $field->params['this']->setObjectparams('form_name', $this->getId() . '.' . $i);
+                    $field->params['this']->setObjectparams('form_ytemplate', $ytemplates);
+                    $field->params['this']->setObjectparams('main_id', $main_id);
+                    $field->params['form_name']       = $field->getName();
+                    $field->params['form_label_type'] = 'html';
+                    $field->params['send']            = false;
+
+                    if ($field->getElement(0) == 'be_manager_relation') {
+                        $field->params['main_table'] = $field->getElement('table');
+                        $field->setName($field->getElement('field'));
+                    }
+                    $field->setValue($rowData[$i] ?: '');
+                    $field->setId($data_index);
+                    $field->enterObject();
+                    $field_output = trim($field->params['form_output'][$field->getId()]);
+                    ?>
+                    <td class="be-value-input type-<?= $column['field']->getElement(0) ?>" data-title="<?= rex_escape($column['label'], 'html_attr') ?>"><?= $field_output ?></td>
                 <?php endforeach ?>
-                <td><a class="btn btn-xs btn-delete" href="javascript:void(0)"><i class="rex-icon rex-icon-delete"></i> <?php echo rex_i18n::msg('yform_delete') ?></a></td>
+                <td class="delete-row"><a class="btn btn-xs btn-delete" href="javascript:void(0)"><i class="rex-icon rex-icon-delete"></i> <?php echo rex_i18n::msg('yform_delete') ?></a></td>
             </tr>
         <?php endforeach ?>
         </tbody>
     </table>
+    <a class="btn btn-primary btn-xs add-mobile-btn" id="<?= $this->getHTMLId() ?>-add-mobile-row" href="javascript:void(0);"><i class="rex-icon rex-icon-add"></i> <?php echo rex_i18n::msg('yform_add_row') ?></a>
 
     <script type="text/javascript">
         (function () {
             var wrapper = jQuery('#<?php echo $this->getHTMLId() ?>'),
-            	be_table_cnt = <?= (int) $data_index ?>;
+                be_table_cnt = <?= (int)$data_index ?>;
 
-            wrapper.find('#<?= $this->getHTMLId() ?>-add-row').click(function () {
-                var tr = $('<tr/>'),
+            wrapper.find('#<?= $this->getHTMLId() ?>-add-row, #<?= $this->getHTMLId() ?>-add-mobile-row').click(function () {
+                var $this = $(this),
+                    $table = $this.parents('.formbe_table').children('table'),
+                    tr = $('<tr/>'),
                     regexp = [
                         // REX_MEDIA
                         new RegExp("(REX_MEDIA_)", 'g'),
@@ -74,42 +88,52 @@ if (count($notice) > 0) {
                         new RegExp("(viewREXMedialist\\()", 'g'),
                     ],
                     row_html = '\
-                    <?php foreach ($columns as $i => $column): ?>\
-                            <td class="be-value-input"><?php
-                        $field = $columns[$i]['field'];
-                        $field->params['this']->setObjectparams('form_name', $this->getId() . '.' . $i);
-                        $field->params['form_name'] = $field->getName();
-                        $field->params['form_label_type'] = 'html';
-                        $field->params['send'] = false;
-                        $field->setValue(null);
-                        $field->setId('{{FIELD_ID}}');
-                        $field->enterObject();
-                        echo strtr($field->params['form_output'][$field->getId()], ["\n" => '', "\r" => '', "'" => "\'"]);
-                        ?></td>\
-                    <?php endforeach ?>\
-                    <td><a class="btn btn-xs btn-delete" href="javascript:void(0)"><i class="rex-icon rex-icon-delete"></i> <?php echo rex_i18n::msg('yform_delete') ?></a></td>\
+                    <?php
+                        foreach ($columns as $i => $column) {
+                            $field = $columns[$i]['field'];
+                            $field->params['form_output'] = [];
+                            $field->params['this']->setObjectparams('form_name', $this->getId() . '.' . $i);
+                            $field->params['this']->setObjectparams('form_ytemplate', $ytemplates);
+                            $field->params['this']->setObjectparams('main_id', $main_id);
+                            $field->params['form_name'] = $field->getName();
+                            $field->params['form_label_type'] = 'html';
+                            $field->params['send'] = false;
+
+                            if ($field->getElement(0) == 'be_manager_relation') {
+                                $field->params['main_table'] = $field->getElement('table');
+                                $field->setName($field->getElement('field'));
+                            }
+                            $field->setValue(null);
+                            $field->setId('{{FIELD_ID}}');
+                            $field->enterObject();
+                            $field_output = trim(strtr($field->params['form_output'][$field->getId()], ["\n" => '', "\r" => '', "'" => "\'"]));
+
+                            echo '<td class="be-value-input type-'. $column['field']->getElement(0) .'" data-title="'. $column['label'] .'">'. $field_output .'</td>';
+                        }
+                        ?>\
+                    <td class="delete-row"><a class="btn btn-xs btn-delete" href="javascript:void(0)"><i class="rex-icon rex-icon-delete"></i> <?php echo rex_i18n::msg('yform_delete') ?></a></td>\
                 ';
 
                 be_table_cnt++;
                 // set new row field ids
                 row_html = row_html.replace(new RegExp('{{FIELD_ID}}', 'g'), be_table_cnt);
+                row_html = row_html.replace(new RegExp('--FIELD_ID--', 'g'), be_table_cnt);
 
                 for (var i in regexp) {
-                    row_html = row_html.replace(regexp[i], '$1'+ be_table_cnt +'<?= $i ?>');
+                    row_html = row_html.replace(regexp[i], '$1' + be_table_cnt + '<?= $i ?>');
                 }
                 tr.html(row_html);
 
                 // replace be medialist
-                tr.find('select[id^="REX_MEDIALIST_"]').each(function() {
+                tr.find('select[id^="REX_MEDIALIST_"]').each(function () {
                     var $select = $(this),
-                        $input  = $select.parent().children('input:first'),
+                        $input = $select.parent().children('input:first'),
                         id = $select.prop('id').replace('REX_MEDIALIST_SELECT_', '');
 
-                    $input.prop('id', 'REX_MEDIALIST_'+ id);
+                    $input.prop('id', 'REX_MEDIALIST_' + id);
                 });
 
-
-                $(this).closest('table').find('tbody').append(tr);
+                $table.find('tbody').append(tr);
                 $(document).trigger('be_table:row-added', [tr]);
                 return false;
             });
