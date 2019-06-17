adae54bf87581b20c9f177b11c8316400929ecd3
Alexander Walther <github.com@alexplus.de>
2019-06-17 15:00:01 +0200

    hidden-Feld: $_SESSION mit abfragen, POST/GET gezielt ansteuern

--- lib/yform/value/hidden.php
+++ lib/yform/value/hidden.php
@@ -13,6 +13,12 @@ class rex_yform_value_hidden extends rex_yform_value_abstract
     {
         if ($this->getElement(3) == 'REQUEST' && isset($_REQUEST[$this->getElement(2)])) {
             $this->value = rex_request($this->getElement(2));
+        } else if ($this->getElement(3) == 'GET' && isset($_REQUEST[$this->getElement(2)])) {
+            $this->value = rex_get($this->getElement(2));
+        } else if ($this->getElement(3) == 'POST' && isset($_REQUEST[$this->getElement(2)])) {
+            $this->value = rex_post($this->getElement(2));
+        } else if ($this->getElement(3) == 'SESSION' && isset($_REQUEST[$this->getElement(2)])) {
+            $this->value = rex_session($this->getElement(2));
         } else {
             $this->value = $this->getElement(2);
         }
@@ -20,7 +26,7 @@ class rex_yform_value_hidden extends rex_yform_value_abstract
 
     public function enterObject()
     {
-        if ($this->needsOutput() && $this->getElement(3) == 'REQUEST') {
+        if ($this->needsOutput() && ($this->getElement(3) == 'REQUEST' || $this->getElement(3) == 'GET' || $this->getElement(3) == 'POST' || $this->getElement(3) == 'SESSION')) {
             $this->params['form_output'][$this->getId()] = $this->parse('value.hidden.tpl.php');
         }
 
@@ -32,6 +38,6 @@ class rex_yform_value_hidden extends rex_yform_value_abstract
 
     public function getDescription()
     {
-        return 'hidden|name|(default)value||[no_db]'."\n".'hidden|job_id|my_id|REQUEST|[no_db]';
+        return 'hidden|name|(default)value||[no_db]'."\n".'hidden|job_id|my_id|REQUEST/GET/POST/SESSION|[no_db]';
     }
 }
