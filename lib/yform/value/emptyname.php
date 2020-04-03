7b898e2fb298ae9b8cc04ceb650f464417b0698f
Nico G <mail@ng-websolutions.de>
2020-04-03 12:56:20 +0200

    Anpassung Feld "emptyname"
    Bugfix: Beim speichern von Datensätze im Table-Manager wurden Datensätze vom Feldtyp "emptyname" in Datenbank geleert.

--- lib/yform/value/emptyname.php
+++ lib/yform/value/emptyname.php
@@ -11,16 +11,7 @@ class rex_yform_value_emptyname extends rex_yform_value_abstract
 {
     public function enterObject()
     {
-        $value = $this->getValue();
-        if (!$value) {
-            $value = '';
-        }
-        $this->setValue($value);
 
-        $this->params['value_pool']['email'][$this->getName()] = $this->getValue();
-        if ($this->saveInDb()) {
-            $this->params['value_pool']['sql'][$this->getName()] = $this->getValue();
-        }
     }
 
     public function getDescription()
