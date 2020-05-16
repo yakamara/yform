5cfedb82b021197cfcc2339337acec4a47aa4577
Jan Kristinus <jan.kristinus@yakamara.de>
2020-05-16 14:44:58 +0200

    installation test if db supports transactions closes #869

--- install.php
+++ install.php
@@ -2,6 +2,15 @@
 
 /** @var rex_addon $this */
 
+try {
+    // transaction test.
+    $testSQL = rex_sql::factory();
+    $testSQL->beginTransaction();
+    $testSQL->commit();
+} catch (Exception $e) {
+    throw new rex_sql_exception('db does not support transactions');
+}
+
 foreach ($this->getInstalledPlugins() as $plugin) {
     // use path relative to __DIR__ to get correct path in update temp dir
     $file = __DIR__.'/plugins/'.$plugin->getName().'/install.php';
