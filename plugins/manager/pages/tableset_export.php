c45de2c6307991db61e4c461f4f4d75938776b65
christophboecker <christoph_boecker@t-online.de>
2021-02-22 17:50:18 +0100

    Verwendet getNameLocalized() für den Anzeigenamen

--- plugins/manager/pages/tableset_export.php
+++ plugins/manager/pages/tableset_export.php
@@ -15,13 +15,7 @@ $page = rex_request('page', 'string', '');
 $yform_tables = [];
 foreach (rex_yform_manager_table::getAll() as $g_table) {
     $table_name = $g_table->getTableName();
-    
-    $name = $g_table->getName();
-    if( $name === $table_name ) {
-        $name = 'translate:'.$table_name;
-    }
-    
-    $yform_tables[$table_name] = rex_i18n::translate($name).' ['.$table_name.']';
+    $yform_tables[$table_name] = rex_i18n::translate($g_table->getNameLocalized()).' ['.$table_name.']';
 }
 
 $yform = new rex_yform();
