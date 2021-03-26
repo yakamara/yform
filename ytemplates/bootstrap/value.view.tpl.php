07efe68ee9a5afc9cddfec3d27c75e29408bbf2f
Jan Kristinus <jan.kristinus@yakamara.de>
2021-03-26 16:28:57 +0100

    view optimiert wenn leeres array oder nur ein Eintrag

--- ytemplates/bootstrap/value.view.tpl.php
+++ ytemplates/bootstrap/value.view.tpl.php
@@ -3,10 +3,17 @@
 $value = $value ?? $this->getValue() ?? '';
 
 if (is_array($value)) {
-    foreach ($value as $k => $v) {
-        $value[$k] = '<li>'.rex_escape($v).'</li>';
+    if (0 == count($value)) {
+        $value = '-';
+    } elseif (1 == count($value)) {
+        $value = rex_escape(current($value));
+    } elseif (1 < count($value)) {
+        foreach ($value as $k => $v) {
+            $value[$k] = '<li>'.rex_escape($v).'</li>';
+        }
+        $value = '<ul>'.implode('', $value).'</ul>';
     }
-    $value = '<ul>'.implode('', $value).'</ul>';
+
 } else {
     $length = strlen($value);
     $title = $value;
@@ -15,19 +22,6 @@ if (is_array($value)) {
         $value = mb_substr($value, 0, $maxsize / 2).' ... '.mb_substr($value, -($maxsize / 2));
     }
     $value = rex_escape($value);
-}
-
-$notice = [];
-if ('' != $this->getElement('notice')) {
-    $notice[] = rex_i18n::translate($this->getElement('notice'), false);
-}
-if (isset($this->params['warning_messages'][$this->getId()]) && !$this->params['hide_field_warning_messages']) {
-    $notice[] = '<span class="text-warning">' . rex_i18n::translate($this->params['warning_messages'][$this->getId()]) . '</span>'; //    var_dump();
-}
-if (count($notice) > 0) {
-    $notice = '<p class="help-block">' . implode('<br />', $notice) . '</p>';
-} else {
-    $notice = '';
 }
 
 $class_group = [];
@@ -41,7 +35,7 @@ $class_label[] = 'control-label';
 echo '
     <div class="'.implode(' ', $class_group).'" id="'.$this->getHTMLId().'">
         <label class="'.implode(' ', $class_label).'" for="'.$this->getFieldId().'">'.$this->getLabel().'</label>
-        <div>' . $value . ' ' . $notice . '</div>
+        <div>' . $value . '</div>
     </div>';
 
 ?>
