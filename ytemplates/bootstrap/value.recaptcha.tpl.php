f70ab300494b318e801806f1dcf03f3e66abe9a7
Jan Kristinus <jan.kristinus@yakamara.de>
2021-06-22 17:14:10 +0200

    classic templates entfernt

--- ytemplates/bootstrap/value.recaptcha.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (1 == $loadScript) {
-    echo '<script src="https://www.google.com/recaptcha/api.js" async defer></script>';
-}
-
-?>
-<div id="<?php echo $this->getHTMLId() ?>" class="g-recaptcha <?php echo $this->getWarningClass(); ?>" data-sitekey="<?php echo $publicKey ?>"></div>
