fb636fd9e0a9924538565c2bb2371ce7ebfd1119
Jan Kristinus <jan.kristinus@yakamara.de>
2021-09-01 19:04:03 +0200

    manager: Tabellen wurden nicht in der Benutzerverwaltung nicht übersetzt #289

--- plugins/manager/lib/yform/manager/table/perm/view.php
+++ plugins/manager/lib/yform/manager/table/perm/view.php
@@ -12,10 +12,15 @@ class rex_yform_manager_table_perm_view extends rex_complex_perm
 
     public static function getFieldParams()
     {
+        $arrayOptions = [];
+        foreach (rex_yform_manager_table::getAll() as $table) {
+            $arrayOptions[$table->getTableName()] = rex_i18n::translate($table->getName()).' ['.$table->getTableName().']';
+        }
+
         return [
             'label' => rex_i18n::msg('yform_manager_table'),
             'all_label' => rex_i18n::msg('yform_manager_tables_view'),
-            'sql_options' => 'select concat(`name`," [", `table_name`,"]") as name, table_name as id from ' . rex::getTablePrefix() . 'yform_table where status = 1 order by prio',
+            'options' => $arrayOptions,
         ];
     }
 }
