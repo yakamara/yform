897f0acfe91ff1d1ab043fbd4b8d8ff8c785e974
Jan Kristinus <jan.kristinus@yakamara.de>
2021-12-03 17:26:50 +0100

    alte remembervalues werden nun bei install aus dem schema entfernt

--- plugins/manager/install.php
+++ plugins/manager/install.php
@@ -103,6 +103,7 @@ if ($addon->isInstalled() && rex_version::compare($addon->getVersion(), '3.6', '
                 case 'captcha':
                 case 'captcha_calc':
                 case 'recaptcha':
+                case 'remembervalues':
                 case 'recaptcha_v3':
                     // remove this fields
                     rex_sql::factory()->setQuery('delete from `' . rex::getTable('yform_field') . '` where id = :id', ['id' => $field['id']]);
