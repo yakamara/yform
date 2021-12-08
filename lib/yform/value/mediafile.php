0ce91a187cbdd3517a58ab4c52e080631da84852
Jan Kristinus <jan.kristinus@yakamara.de>
2021-12-08 18:27:28 +0100

    mediafile Umbau auf Serviceklassen vom mediapool #1069

--- lib/yform/value/mediafile.php
+++ lib/yform/value/mediafile.php
@@ -23,18 +23,20 @@ class rex_yform_value_mediafile extends rex_yform_value_abstract
 
         $mediapool_user = ('' == $this->getElement('user')) ? 'yform::mediafile' : $this->getElement('user');
         $pool = $this->params['value_pool']['email'];
-        $mediapool_user = preg_replace_callback('/###(\w+)###/',
-                                                            static function ($m) use ($pool) {
+        $mediapool_user = preg_replace_callback(
+            '/###(\w+)###/',
+            static function ($m) use ($pool) {
                                                                 return $pool[$m[1]]
                                                                          ?? 'key not found';
                                                             },
-                                                            $mediapool_user);
+            $mediapool_user
+        );
 
         $sizes = array_map('intval', explode(',', $this->getElement('max_size')));
         $minsize = count($sizes) > 1 ? (int) ($sizes[0] * 1024) : 0;
         $maxsize = count($sizes) > 1 ? (int) ($sizes[1] * 1024) : (int) ($sizes[0] * 1024);
 
-        $error = [];
+        $warnings = [];
         $err_msgs = explode(',', $this->getElement('messages')); // min_err,max_err,type_err,empty_err
         $err_msgs['min_err'] = $err_msgs[0];
         $err_msgs['max_err'] = $err_msgs[1] ?? $err_msgs[0];
@@ -51,10 +53,10 @@ class rex_yform_value_mediafile extends rex_yform_value_abstract
         // SIZE CHECK
         if ($this->params['send'] && isset($_FILES[$rfile]) && '' != $_FILES[$rfile]['name'] && ($_FILES[$rfile]['size'] > $maxsize || $_FILES[$rfile]['size'] < $minsize)) {
             if ($_FILES[$rfile]['size'] < $minsize) {
-                $error[] = $err_msgs['min_err'];
+                $warnings[] = $err_msgs['min_err'];
             }
             if ($_FILES[$rfile]['size'] > $maxsize) {
-                $error[] = $err_msgs['max_err'];
+                $warnings[] = $err_msgs['max_err'];
             }
             unset($_FILES[$rfile]);
             $this->setValue('');
@@ -66,26 +68,37 @@ class rex_yform_value_mediafile extends rex_yform_value_abstract
             }
 
             if (isset($_FILES[$rfile]) && '' != $_FILES[$rfile]['name']) {
-                $FILE['size'] = $_FILES[$rfile]['size'];
-                $FILE['name'] = $_FILES[$rfile]['name'];
-                $FILE['type'] = $_FILES[$rfile]['type'];
-                $FILE['tmp_name'] = $_FILES[$rfile]['tmp_name'];
-                $FILE['error'] = $_FILES[$rfile]['error'];
+                $file = $_FILES[$rfile];
 
-                // EXTENSION CHECK
-                $extensions_array = explode(',', $this->getElement(4));
-                $ext = '.' . pathinfo($FILE['name'], PATHINFO_EXTENSION);
-                if (!in_array(mb_strtolower($ext), $extensions_array) && !in_array(mb_strtoupper($ext), $extensions_array)) {
-                    $error[] = $err_msgs['type_err'];
-                } else {
-                    $NEWFILE = $this->saveMedia($FILE, rex_path::media(), $extensions_array, $media_category_id, $mediapool_user);
+                $data = [];
+                $data['title'] = (string) ($file['name'] ?? '');
+                $data['category_id'] = (int) $media_category_id;
+                $data['file']['name'] = (string) ($file['name'] ?? '');
+                $data['file']['tmp_path'] = (string) ($file['tmp_name'] ?? '');
+                $data['file']['path'] = (string) ($file['tmp_name'] ?? '');
 
-                    if ($NEWFILE['ok']) {
-                        $this->setValue($NEWFILE['filename']);
+                // TODO: im Frontend noch prüfen. Mediafile nicht ersetzen wenn md5 identisch
+                $doSubindexing = true;
+
+                // TODO: wenn validierungs abbruch, noch nicht speichern
+
+                $allowedExtensions = [];
+                if ('' != $this->getElement('types')) {
+                    $allowedExtensions['types'] = $this->getElement('types');
+                }
+                $warnings = [];
+
+                try {
+                    $return = rex_media_service::addMedia($data, $doSubindexing, $allowedExtensions);
+                    if (1 == $return['ok']) {
+                        $this->setValue($return['filename']);
                     } else {
+                        $warnings = $return['messages'];
                         $this->setValue('');
-                        $error[] = 'unknown_save_error';
                     }
+                } catch (rex_api_exception $e) {
+                    $warnings[] = $e->getMessage();
+                    $this->setValue('');
                 }
             }
         }
@@ -99,12 +112,12 @@ class rex_yform_value_mediafile extends rex_yform_value_abstract
 
         //# check for required file
         if ($this->params['send'] && 1 == $this->getElement('required') && '' == $this->getValue()) {
-            $error[] = $err_msgs['empty_err'];
+            $warnings[] = $err_msgs['empty_err'];
         }
 
-        if ($this->params['send'] && count($error) > 0) {
+        if ($this->params['send'] && count($warnings) > 0) {
             $this->params['warning'][$this->getId()] = $this->params['error_class'];
-            $this->params['warning_messages'][$this->getId()] = implode(', ', $error);
+            $this->params['warning_messages'][$this->getId()] = implode(', ', $warnings);
         }
 
         if ($this->needsOutput() && $this->isViewable()) {
@@ -142,91 +155,6 @@ class rex_yform_value_mediafile extends rex_yform_value_abstract
             'db_type' => ['text'],
             'multi_edit' => false,
         ];
-    }
-
-    public function saveMedia($FILE, $filefolder, $extensions_array, $rex_media_category_id, $mediapool_user)
-    {
-        $FILENAME = $FILE['name'];
-        $FILESIZE = $FILE['size'];
-        $FILETYPE = $FILE['type'];
-
-        $NFILENAME = mb_strtolower(preg_replace('/[^a-zA-Z0-9.\-\$\+]/', '_', $FILENAME));
-        if ('' != strrpos($NFILENAME, '.')) {
-            $NFILE_NAME = mb_substr($NFILENAME, 0, mb_strlen($NFILENAME) - (mb_strlen($NFILENAME) - mb_strrpos($NFILENAME, '.')));
-            $NFILE_EXT = mb_substr($NFILENAME, mb_strrpos($NFILENAME, '.'), mb_strlen($NFILENAME) - mb_strrpos($NFILENAME, '.'));
-        } else {
-            $NFILE_NAME = $NFILENAME;
-            $NFILE_EXT = '';
-        }
-
-        $ERROR_EXT = ['.php', '.php3', '.php4', '.php5', '.phtml', '.pl', '.asp', '.aspx', '.cfm'];
-        if (in_array($NFILE_EXT, $ERROR_EXT)) {
-            $NFILE_NAME .= $NFILE_EXT;
-            $NFILE_EXT = '.txt';
-        }
-
-        $standard_extensions_array = ['.rtf', '.pdf', '.doc', '.gif', '.jpg', '.jpeg'];
-        if (0 == count($extensions_array)) {
-            $extensions_array = $standard_extensions_array;
-        }
-
-        if (!in_array($NFILE_EXT, $extensions_array)) {
-            $RETURN = false;
-            $RETURN['ok'] = false;
-            return $RETURN;
-        }
-
-        $NFILENAME = $NFILE_NAME . $NFILE_EXT;
-
-        if (file_exists($filefolder . '/' . $NFILENAME)) {
-            for ($cf = 1; $cf < 1000; ++$cf) {
-                $NFILENAME = $NFILE_NAME . '_' . $cf . $NFILE_EXT;
-                if (!file_exists($filefolder . '/' . $NFILENAME)) {
-                    break;
-                }
-            }
-        }
-
-        $message = '';
-        if (!move_uploaded_file($FILE['tmp_name'], $filefolder . "/$NFILENAME")) {
-            if (!copy($FILE['tmp_name'], $filefolder . '/' . $NFILENAME)) {
-                $message = 'move file $NFILENAME failed | ';
-                $RETURN = false;
-                $RETURN['ok'] = false;
-                return $RETURN;
-            }
-        }
-
-        @chmod($filefolder . '/' . $NFILENAME, rex::getFilePerm());
-        $RETURN['type'] = $FILETYPE;
-        $RETURN['msg'] = $message;
-        $RETURN['ok'] = true;
-        $RETURN['filename'] = $NFILENAME;
-
-        $saveSQL = rex_sql::factory()
-            ->setTable(rex::getTablePrefix() . 'media')
-            ->setValue('title', '')
-            ->setValue('filetype', $FILETYPE)
-            ->setValue('filename', $NFILENAME)
-            ->setValue('originalname', $FILENAME)
-            ->setValue('filesize', $FILESIZE)
-            ->setValue('category_id', $rex_media_category_id)
-            ->setValue('createdate', date(rex_sql::FORMAT_DATETIME))
-            ->setValue('createuser', $mediapool_user)
-            ->setValue('updatedate', date(rex_sql::FORMAT_DATETIME))
-            ->setValue('updateuser', $mediapool_user);
-
-        // get widht height
-        $size = @getimagesize($filefolder . '/' . $NFILENAME);
-
-        if ($size) {
-            $saveSQL->setValue('width', $size[0]);
-            $saveSQL->setValue('height', $size[1]);
-        }
-
-        $saveSQL->insert();
-
-        return $RETURN;
     }
 
     public static function getSearchField($params)
