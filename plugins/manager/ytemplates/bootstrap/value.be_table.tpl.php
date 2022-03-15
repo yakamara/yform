9bae92d1425ee97079b45c0c3af08bfef1faa787
Alexander Walther <github.com@alexplus.de>
2022-03-15 10:50:13 +0100

    Aktions-Button: Größe von xs auf sm
    Die Usability ist echt nicht so toll, wenn der Button so klein ist.

--- plugins/manager/ytemplates/bootstrap/value.be_table.tpl.php
+++ plugins/manager/ytemplates/bootstrap/value.be_table.tpl.php
@@ -36,7 +36,7 @@ $main_id = $this->params['this']->getObjectparams('main_id');
             <?php foreach ($columns as $column): ?>
                 <th class="type-<?= $column['field']->getElement(0) ?>"><?php echo htmlspecialchars($column['label']) ?></th>
             <?php endforeach ?>
-            <th class="rex-table-action"><a class="btn btn-xs btn-primary" id="<?= $this->getHTMLId() ?>-add-row" href="javascript:void(0);"><i class="rex-icon rex-icon-add"></i> <?php echo rex_i18n::msg('yform_add_row') ?></a></th>
+            <th class="rex-table-action"><a class="btn btn-sm btn-primary" id="<?= $this->getHTMLId() ?>-add-row" href="javascript:void(0);"><i class="rex-icon rex-icon-add"></i> <?php echo rex_i18n::msg('yform_add_row') ?></a></th>
         </tr>
         </thead>
         <tbody>
