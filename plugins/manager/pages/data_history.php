cdc55bd4e53a67a07e947d4f7b496ea91537f9e9
Jan Kristinus <jan.kristinus@yakamara.de>
2022-09-12 12:13:33 +0200

    Suche nach Id, User, Action und Dqtum in Historie eingebaut

--- plugins/manager/pages/data_history.php
+++ plugins/manager/pages/data_history.php
@@ -10,6 +10,11 @@ $filterDataset = rex_request('filter_dataset', 'bool');
 $historyId = rex_request('history_id', 'int');
 $_csrf_key = $_csrf_key ?? '';
 
+$historySearchId = rex_request('historySearchId', 'int', null);
+$historySearchDate = rex_request('historySearchDate', 'string', null);
+$historySearchUser = rex_request('historySearchUserId', 'int', null);
+$historySearchAction = rex_request('historySearchAction', 'string', null);
+
 $dataset = null;
 if ($datasetId) {
     $dataset = rex_yform_manager_dataset::getRaw($datasetId, $this->table->getTableName());
@@ -20,8 +25,28 @@ if ($datasetId) {
 $filterWhere = '';
 if ($filterDataset) {
     echo rex_view::info('<b>' . rex_i18n::msg('yform_history_dataset_id') .':</b> ' . $datasetId);
-
     $filterWhere = ' AND dataset_id = '.$datasetId;
+}
+
+if ($historySearchId) {
+    $filterWhere .= ' AND dataset_id = ' . $historySearchId;
+}
+
+if ($historySearchDate) {
+    $historyDateObject = DateTime::createFromFormat('Y-m-d', $historySearchDate);
+    if (!$historyDateObject) {
+        $historyDateObject = new DateTime();
+    }
+    $historyDateObject->modify('+1 day');
+    $filterWhere .= ' AND timestamp <= ' . rex_sql::factory()->escape($historyDateObject->format('Y-m-d'));
+}
+
+if ($historySearchUser) {
+    $filterWhere .= ' AND user =' . rex_sql::factory()->escape($historySearchUser);
+}
+
+if ($historySearchAction) {
+    $filterWhere .= ' AND action =' . rex_sql::factory()->escape($historySearchAction);
 }
 
 if ('view' === $subfunc && $dataset && $historyId) {
@@ -121,17 +146,28 @@ if (rex::getUser()->isAdmin() && in_array($subfunc, ['delete_old', 'delete_all']
 }
 
 $sql = rex_sql::factory();
-$list = rex_list::factory(
-    'SELECT
+
+$listQuery = 'SELECT
         h.id, dataset_id,
-        IF(LENGTH(hf.value) > 50, CONCAT(LEFT(hf.value, 50), "…"), hf.value) as title,
+        id as title,
         `action`, `user`, `timestamp`
     FROM '.rex::getTable('yform_history').' h
-    LEFT JOIN '.rex::getTable('yform_history_field').' hf ON hf.history_id = h.id AND hf.field IN ("title", "titel", "name", "last_name")
-    WHERE `table_name` = '.$sql->escape($this->table->getTableName()).$filterWhere.'
+    WHERE
+        `table_name` = '.$sql->escape($this->table->getTableName()).
+    $filterWhere.'
     GROUP BY h.id
-    ORDER BY `timestamp` DESC'
-);
+    ORDER BY `timestamp` DESC';
+
+$userQuery = 'SELECT
+        distinct `user`
+    FROM '.rex::getTable('yform_history').' h
+    WHERE
+        `table_name` = '.$sql->escape($this->table->getTableName());
+
+$list = rex_list::factory($listQuery);
+
+$users = $sql->getArray($userQuery);
+$users = array_combine(array_column($users, 'user'), array_column($users, 'user'));
 
 $list->addParam('table_name', $this->table->getTableName());
 $list->addParam('func', 'history');
@@ -142,10 +178,39 @@ if ($filterDataset) {
     $list->addParam('dataset_id', $datasetId);
 }
 
+if ($historySearchId) {
+    $list->addParam('historySearchId', $historySearchId);
+}
+
+if ($historySearchDate) {
+    $list->addParam('historySearchDate', $historySearchDate);
+}
+
+if ($historySearchUser) {
+    $list->addParam('historySearchUser', $historySearchUser);
+}
+
+if ($historySearchAction) {
+    $list->addParam('historySearchAction', $historySearchAction);
+}
+
 $list->removeColumn('id');
 
 $list->setColumnLabel('dataset_id', rex_i18n::msg('yform_history_dataset_id'));
 $list->setColumnLabel('title', rex_i18n::msg('yform_history_dataset'));
+$list->setColumnFormat('title', 'custom', static function (array $params) {
+    $result = rex_sql::factory()->getArray('select * from '.rex::getTable('yform_history_field').' where history_id=:history_id and field IN ("title", "titel", "name", "last_name") LIMIT 1', [
+        'history_id' => $params['value'],
+    ]);
+    $title = '[no title found]';
+    if (isset($result[0])) {
+        $title = $result[0]['value'];
+        if (mb_strlen($title) > 50) {
+            $title = substr($title, 0, 50). '…';
+        }
+    }
+    return rex_escape($title);
+});
 
 $list->setColumnLabel('action', rex_i18n::msg('yform_history_action'));
 $list->setColumnFormat('action', 'custom', static function (array $params) {
@@ -200,11 +265,64 @@ if (rex::getUser()->isAdmin()) {
     $options = '<small class="rex-panel-option-title">' . rex_i18n::msg('yform_history_delete') . ':</small> ' . $fragment->parse('core/buttons/button_group.php');
 }
 
+$historySearchForm = new rex_yform();
+$historySearchForm->setObjectparams('form_action', $list->getUrl());
+$historySearchForm->setObjectparams('form_showformafterupdate', true);
+$historySearchForm->setObjectparams('real_field_names', true);
+$historySearchForm->setObjectparams('csrf_protection', false);
+$historySearchForm->setHiddenField('_csrf_token', rex_csrf_token::factory($_csrf_key)->getValue());
+
+if (!$datasetId) {
+    $historySearchForm->setValueField('text', [
+        'name' => 'historySearchId',
+        'label' => 'id',
+    ]);
+}
+
+$historySearchForm->setValueField('date', [
+    'name' => 'historySearchDate',
+    'label' => 'Date',
+    'widget' => 'input:text',
+    'current_date' => true,
+    'notice' => rex_i18n::msg('yform_manager_history_date_notice'),
+    'attributes' => '{"data-yform-tools-datepicker":"YYYY-MM-DD"}',
+]);
+$historySearchForm->setValueField('choice', [
+    'name' => 'historySearchAction',
+    'label' => 'Action',
+    'choices' => [
+        '' => rex_i18n::msg('yform_manager_actions_all'),
+        rex_yform_manager_dataset::ACTION_CREATE => rex_i18n::msg('yform_history_action_'.rex_yform_manager_dataset::ACTION_CREATE),
+        rex_yform_manager_dataset::ACTION_UPDATE => rex_i18n::msg('yform_history_action_'.rex_yform_manager_dataset::ACTION_UPDATE),
+        rex_yform_manager_dataset::ACTION_DELETE => rex_i18n::msg('yform_history_action_'.rex_yform_manager_dataset::ACTION_DELETE),
+    ],
+]);
+
+$historySearchForm->setValueField('choice', [
+    'name' => 'historySearchUser',
+    'label' => 'User',
+    'choices' => array_merge(
+        ['' => rex_i18n::msg('yform_manager_users_all')],
+        $users
+    ),
+]);
+
+$fragment = new rex_fragment();
+$fragment->setVar('class', 'edit', false);
+$fragment->setVar('title', rex_i18n::msg('yform_manager_search'));
+$fragment->setVar('body', $historySearchForm->getForm(), false);
+$searchForm = $fragment->parse('core/page/section.php');
+
 $fragment = new rex_fragment();
 $fragment->setVar('title', rex_i18n::msg('yform_history'));
 $fragment->setVar('options', $options, false);
 $fragment->setVar('content', $content, false);
-echo $fragment->parse('core/page/section.php');
+$searchList = $fragment->parse('core/page/section.php');
+
+echo '<div class="row">';
+echo '<div class="col-sm-3 col-md-3 col-lg-2">' . $searchForm . '</div>';
+echo '<div class="col-sm-9 col-md-9 col-lg-10">' . $searchList . '</div>';
+echo '</div>';
 
 ?>
 <div class="modal fade" id="rex-yform-history-modal" tabindex="-1" role="dialog" aria-hidden="true">
