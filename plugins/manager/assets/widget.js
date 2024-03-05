b14a066a60c17295f3051f4eb421c2e3f4b2d0c6
Yves Torres <ynamite@bluewin.ch>
2024-03-05 11:38:28 +0100

    Update widget.js with rex:YForm_selectData
    Fixes https://github.com/yakamara/yform/issues/1483

--- plugins/manager/assets/widget.js
+++ plugins/manager/assets/widget.js
@@ -147,6 +147,12 @@ $(document).on('rex:ready',function() {
                     let multiple = this.dataset.multiple;
                     let value = this.dataset.value;
 
+                    var event = opener.jQuery.Event('rex:YForm_selectData')
+                    opener.jQuery(window).trigger(event, [id, value, multiple])
+                    if (!event.isDefaultPrevented()) {
+                      self.close()
+                    }
+                    
                     if(multiple == "1") {
                         let viewObject = opener.document.getElementById('yform-dataset-view-'+opener_id);
                         let option = opener.document.createElement("OPTION");
