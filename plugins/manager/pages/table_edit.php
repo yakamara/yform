cdb4b34ff4a0fa2fa9f18916b0a73db69f34586a
Thomas Skerbis <thomas.skerbis@mac.com>
2024-08-22 14:55:20 +0200

    csfix table_edit.php

--- plugins/manager/pages/table_edit.php
+++ plugins/manager/pages/table_edit.php
@@ -122,7 +122,7 @@ if ('tableset_import' == $func && rex::getUser()->isAdmin()) {
     $yform->setValidateField('empty', ['name', rex_i18n::msg('yform_manager_table_enter_name')]);
     $yform->setValueField('textarea', ['description', '<br />' . rex_i18n::msg('yform_manager_table_description'), 'attributes' => '{"class":"form-control yform-textarea-short"}']);
     $yform->setValueField('text', ['table_icon', rex_i18n::msg('yform_manager_custom_icon')]);
-    
+
     $yform->setValueField('html', ['html' => '</div><div class="col-md-6">']);
 
     $yform->setValueField('html', ['html' => '<label>' . rex_i18n::msg('yform_manager_table_func') . '</label>']);
