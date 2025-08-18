f60e195dab689e6fea998428d85fa9edc2d56cb4
christophboecker <christoph_boecker@t-online.de>
2025-08-18 17:31:31 +0200

    Plugins "Tools" beim Update auf 5.x löschen (#1573)
    * Plugins "Tools" beim Update auf 5.x löschen
    
    siehe #1572
    
    * Plugin-Verzeichnis komplett löschen

--- update.php
+++ update.php
@@ -1,9 +1,6 @@
 <?php
 
-rex_dir::delete(rex_path::plugin('yform', 'email'));
-rex_dir::delete(rex_path::plugin('yform', 'rest'));
-rex_dir::delete(rex_path::plugin('yform', 'docs'));
-rex_dir::delete(rex_path::plugin('yform', 'manager'));
+rex_dir::delete(rex_path::addon('yform','plugins'),true);
 
 /** @var rex_addon $this */
 $this->includeFile(__DIR__ . '/install.php');
